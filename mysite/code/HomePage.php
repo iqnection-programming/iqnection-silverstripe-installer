a0811d3ebae83d2d40727c7b6c326c2d99115820
App1e5auce <mensch.cory@gmail.com>
2015-04-30 11:52:28 -0400

    Accidentally included banner content.

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -2,13 +2,11 @@
 	class HomePage extends Page {
 	
 		private static $db = array(
-			"BannerContent" => "HTMLText"
 		);
 		
 		public function getCMSFields()
 		{
 			$fields = parent::getCMSFields();
-			$fields->addFieldToTab("Root.Content.Main", new HTMLEditorField("BannerContent", "Banner Content"),"Content");
 			return $fields;
 		}
 	}
