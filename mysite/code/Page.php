90796e912728fd0b5cefd0600f4768095427abcd
meckert <meckert@iqnection.com>
2015-05-11 16:36:29 -0400

    301 redirects part 1

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,19 +1,41 @@
 <?php
-class Page extends SiteTree {
-
-	private static $db = array(
-	);
-
-	private static $has_one = array(
-	);
-
-}
-class Page_Controller extends ContentController {
+	class Page extends SiteTree 
+	{
 	
-	private static $allowed_actions = array (
-	);
-
-	public function init() {
-		parent::init();
+		private static $db = array(
+		);
+	
+		private static $has_one = array(
+		);
+	
+		function onBeforeWrite()
+		{
+			if ($URLRedirects = $this->URLRedirects)
+			{
+				$RedirectURLs = explode("\n",$URLRedirects);
+				$NewRedirectURLs = array();
+				foreach($RedirectURLs as $RedirectURL)
+				{
+					$NewRedirectURL = preg_replace("#".addslashes(Director::absoluteBaseURL())."#","",$RedirectURL);
+					// make sure there's a slash at the begining
+					if (substr($NewRedirectURL,0,1) != "/") $NewRedirectURL = "/".$NewRedirectURL;
+					str_replace("//","/",$NewRedirectURL);
+					$NewRedirectURLs[] = $NewRedirectURL;
+				}
+				$this->URLRedirects = implode("\n",$NewRedirectURLs);
+			}
+			parent::onBeforeWrite();
+		}
 	}
-}
+	
+	class Page_Controller extends ContentController 
+	{
+		
+		private static $allowed_actions = array (
+		);
+	
+		public function init()
+		{
+			parent::init();
+		}
+	}
