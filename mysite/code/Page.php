007373f250c51ddf79d2596c5aa8e1abffb6688f
App1e5auce <mensch.cory@gmail.com>
2015-05-13 08:21:19 -0400

    Moved, added hook!

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -10,24 +10,12 @@
 		private static $has_one = array(
 		);
 	
-		function onBeforeWrite()
+		public function onBeforeWrite()
 		{
-			if ($URLRedirects = $this->URLRedirects)
-			{
-				$RedirectURLs = explode("\n",$URLRedirects);
-				$NewRedirectURLs = array();
-				foreach($RedirectURLs as $RedirectURL)
-				{
-					$NewRedirectURL = preg_replace("#".addslashes(Director::absoluteBaseURL())."#","",$RedirectURL);
-					// make sure there's a slash at the begining
-					if (substr($NewRedirectURL,0,1) != "/") $NewRedirectURL = "/".$NewRedirectURL;
-					str_replace("//","/",$NewRedirectURL);
-					$NewRedirectURLs[] = $NewRedirectURL;
-				}
-				$this->URLRedirects = implode("\n",$NewRedirectURLs);
-			}
+			$this->extend('hook_onBeforeWrite', $this);
 			parent::onBeforeWrite();
 		}
+		
 	}
 	
 	class Page_Controller extends ContentController 
