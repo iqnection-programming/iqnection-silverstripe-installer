21f4b32d238772fe36e433724d418549fbf471d8
App1e5auce <mensch.cory@gmail.com>
2015-05-14 08:02:48 -0400

    Adding hooks for things :(

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,31 +1,23 @@
 <?php
 
-
 	class Page extends SiteTree 
 	{
-	
-		private static $db = array(
-		);
-	
-		private static $has_one = array(
-		);
-	
+			
 		public function onBeforeWrite()
 		{
 			$this->extend('hook_onBeforeWrite', $this);
 			parent::onBeforeWrite();
 		}
-		
-	}
 	
+	}
+
 	class Page_Controller extends ContentController 
 	{
 		
-		private static $allowed_actions = array (
-		);
+		public $templates;
 	
-		public function init()
-		{
+		public function init() {
 			parent::init();
 		}
+		
 	}
