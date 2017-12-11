575340ba87885507fa14d8d40b5cd8a4851c8145
iqmeckert <meckert@iqnection.com>
2017-12-11 14:35:35 -0500

    trying base config

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,2 +1,10 @@
 <?php
 
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+
+use SilverStripe\Core\EnvironmentLoader;
+if(file_exists(BASE_PATH.'/mysite/.env'))
+{
+	$loader = new EnvironmentLoader();
+	$loader->loadFile(BASE_PATH.'/mysite/.env');
+}
